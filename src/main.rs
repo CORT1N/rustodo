9da8544e77b134052b1f44f87f5e145b5c5a6b40
Lucas Bonnaire <lbonnairepro@gmail.com>
2025-10-28 14:18:46 +0100

    added cli manual gestion

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::env;
+
+fn check_args_length(command: &str, args: &Vec<String>) {}
+
+fn add_task() {}
+
+fn remove_task() {}
+
+fn list_tasks() {}
+
+fn mark_task_done() {}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 { panic!("Usage: rustodo <command>"); }
+    let command = args[1].as_str();
+    match command {
+        "add" => {
+            if args.len() < 3 {
+                panic!("Usage: rustodo add <title of your task>");
+            }
+            println!("Action: {}, Arg: {}", command, args[2..].to_vec().join(" "));
+        }
+        "remove" => {
+            if args.len() < 3 {
+                panic!("Usage: rustodo remove <ID of your task>");
+            }
+            println!("Action: {}, Arg: {}", command, args[2]);
+        }
+        "list" => println!("Action: {}", command),
+        "done" => {
+            if args.len() < 3 {
+                panic!("Usage: rustodo done <ID of your task>");
+            }
+            println!("Action: {}, Arg: {}", command, args[2]);
+        }
+        _ => println!("Unknown command"),
+    }
 }
